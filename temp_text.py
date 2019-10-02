1f964f860910fb1631bba872828b9ffd37ecdcf6
Henry McCreery <henrymccy@gmail.com>
2019-10-02 08:49:58 -0700

    update temperature sensor

--- temp_text.py
+++ temp_text.py
@@ -9,11 +9,12 @@ sensor=Adafruit_DHT.DHT11
  
 # Set GPIO sensor is connected to
 gpio=17
-temperature = 0
-humidity = 0
+temperature = 0.0
+humidity = 0.0
 # Use read_retry method. This will retry up to 15 times to
 # get a sensor reading (waiting 2 seconds between each retry).
 def get_temp():
+    global humidity, temperature
     humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
  
 # Reading the DHT11 is very sensitive to timings and occasionally
